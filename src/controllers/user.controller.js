0f5330e9ae0b605dcedc29058ca0de5e7287903b
Shahi77 <shahiinfinity02@gmail.com>
2024-04-04 12:08:57 +0530

    fix error when cover image not uploaded

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,7 @@ import { User } from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { username, email, fullName, password } = req.body;
-  console.log("email:", email);
+  // console.log("email:", email);
   // check if any field is empty
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -22,7 +22,16 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
